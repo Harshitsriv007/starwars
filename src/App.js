32c52ffbe314c2b5bf882775b66b888d16455a1d
Harshitsriv007 <harshit.srivastv726@gmail.com>
2023-07-18 03:37:44 +0530

    Routes handle

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import './App.css';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, redirect } from 'react-router-dom';
 import Login from './login';
 import Planet from './planet';
+
 
 function App() {
   return (
     <div>
       <Routes>
         <Route path="/" element={<Login />} />
+        <Route path="/starwars" element={<Navigate to="/" replace={true}/>}
         <Route path='/planet' element={<Planet/>} />
       </Routes>
     </div>
